b6a6dccd6eec7e73d51b91015028876186925648
Quartz <skyu.dev@gmail.com>
2023-01-03 20:44:17 +0900

    chore: add dummy data and globalstyle

---
+++ src/utils/dummyData.js
@@ -0,0 +1,56 @@
+import { KANBAN_STATE } from './constant';
+
+export const users = [
+  '유서경',
+  '이수창',
+  '경지윤',
+  '김수진',
+  '임수진',
+  '차지환',
+  '김형욱',
+];
+
+export const kanbanBoard = {
+  [KANBAN_STATE.TODOS]: [
+    {
+      id: 0,
+      title: '팀프로젝트 회의',
+      content: 'Best practice 찾기',
+      dueDate: '2023-01-12T19:30',
+    },
+    {
+      id: 6,
+      title: '팀프로젝트 회의',
+      content: 'Best practice 찾기',
+      dueDate: '2023-01-12T19:30',
+    },
+  ],
+  [KANBAN_STATE.PROGRESS]: [
+    {
+      id: 1,
+      title: '팀프로젝트 회의',
+      content: 'Best practice 찾기',
+      dueDate: '2023-01-12T19:30',
+    },
+    {
+      id: 2,
+      title: '팀프로젝트 회의',
+      content: 'Best practice 찾기',
+      dueDate: '2023-01-12T19:30',
+    },
+  ],
+  [KANBAN_STATE.DONE]: [
+    {
+      id: 3,
+      title: '팀프로젝트 회의',
+      content: 'Best practice 찾기',
+      dueDate: '2023-01-12T19:30',
+    },
+    {
+      id: 4,
+      title: '팀프로젝트 회의',
+      content: 'Best practice 찾기',
+      dueDate: '2023-01-12T19:30',
+    },
+  ],
+};
