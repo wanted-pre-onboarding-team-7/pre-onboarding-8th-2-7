ca0bc5da94526c3337e430c3c59cb3360283aa71
Quartz <skyu.dev@gmail.com>
2023-01-03 23:43:40 +0900

    Style: theme 색상 추가

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,12 @@
-export const theme = {};
+import { KANBAN_STATE } from './utils/constant';
+
+export const theme = {
+  [KANBAN_STATE.TODOS]: '#979795',
+  [KANBAN_STATE.PROGRESS]: '#5c97bf',
+  [KANBAN_STATE.DONE]: '#5d9376',
+  background: '#ffffff',
+  shadow: '#e3e3e3',
+  border: '#eeeeec',
+  text: '#9d9998',
+  button: '#2282e2',
+};
