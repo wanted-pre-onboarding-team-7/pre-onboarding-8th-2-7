4a3848aa6d702725dc700d4cde7575f372adc3f6
khw970421 <khw970421@kakao.com>
2023-01-04 16:41:34 +0900

    Refactor: kanban 상태값 하위 컴포넌트에서 처리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react';
-
 import KanbanContainer from './../components/kanban/KanbanContainer';
-import { kanbanBoard } from '../utils/dummyData';
 
 const Home = () => {
-  const [kanban, setKanban] = useState({});
-
-  useEffect(() => {
-    setKanban(kanbanBoard);
-  }, []);
-
   return (
     <div>
-      <KanbanContainer kanban={kanban} />
+      <KanbanContainer />
     </div>
   );
 };
