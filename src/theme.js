3153988233059072ab2b6446b6854dee9df86408
jiyoonZ <wldbsl416@gmail.com>
2023-01-04 15:48:18 +0900

    Style : 테마컬러 지정
    상태별 컬러, 버튼 컬러, 카드컬러

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,8 @@
-export const theme = {};
+export const theme = {
+  cardColor: '#F2F2F2',
+  bgColor: '#D9D9D9',
+  doneColor: '#F25050',
+  processColor: '#F2D230',
+  todosColor: '#A7D93B',
+  btnColor: '#3098F2',
+};
