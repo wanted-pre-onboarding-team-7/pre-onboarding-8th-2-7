a0e8a70f73996d24c0ed435bb22e689cd63399c3
khw970421 <khw970421@kakao.com>
2023-01-04 16:49:23 +0900

    Feat: 고유번호순 정렬처리

--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -1,24 +1,31 @@
 import KanbanItem from './KanbanItem';
 import React from 'react';
+import styled from 'styled-components';
 
 const Kanban = ({ status, items, clickKanban }) => {
   const clickKanbanItem = (item) => clickKanban(item, status);
   return (
-    <>
+    <KanbanContainer>
       <h3>{status}</h3>
-      {items.map(({ id, title, content, dueDate, manager }) => (
-        <KanbanItem
-          id={id}
-          title={title}
-          content={content}
-          dueDate={dueDate}
-          manager={manager}
-          clickKanban={clickKanbanItem}
-          key={id}
-        />
-      ))}
-    </>
+      {items
+        .sort((a, b) => a.id - b.id)
+        .map(({ id, title, content, dueDate, manager }) => (
+          <KanbanItem
+            id={id}
+            title={title}
+            content={content}
+            dueDate={dueDate}
+            manager={manager}
+            clickKanban={clickKanbanItem}
+            key={id}
+          />
+        ))}
+    </KanbanContainer>
   );
 };
+
+const KanbanContainer = styled.div`
+  margin: 10px;
+`;
 
 export default Kanban;
