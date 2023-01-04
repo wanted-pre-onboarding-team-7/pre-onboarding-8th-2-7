0312a33ff7120269a7dc95d52b0cdc8434d4f402
khw970421 <khw970421@kakao.com>
2023-01-04 18:23:38 +0900

    Feat: 담당자 기능 구현

--- src/components/kanban/modal/Charge.jsx
+++ src/components/kanban/modal/Charge.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Charge = () => {
-  return <div></div>;
+const Charge = ({ searchArr, changeInputValue }) => {
+  return (
+    <div>
+      <Input type="text" onChange={changeInputValue} />
+      <Ul>
+        {searchArr.map((v) => (
+          <Li>{v}</Li>
+        ))}
+      </Ul>
+    </div>
+  );
 };
+
+const Input = styled.input`
+  width: 200px;
+`;
+
+const Ul = styled.ul`
+  width: 200px;
+  position: absolute;
+  background-color: white;
+  border: 1px solid black;
+`;
+
+const Li = styled.li`
+  padding: 5px 10px;
+`;
 
 export default Charge;
--- src/components/kanban/modal/Modal.jsx
+++ src/components/kanban/modal/Modal.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Charge from './Charge';
 import React from 'react';
 import styled from 'styled-components';
+import { users } from '../../../utils/dummyData';
 
 const Modal = ({
   modalKanbanItem,
@@ -14,44 +15,84 @@ const Modal = ({
   const titleRef = useRef();
   const dueDateRef = useRef();
 
+  const [searchArr, setSearchArr] = useState([]);
+
   useEffect(() => {
     titleRef.current.value = title || '';
     dueDateRef.current.value = dueDate;
   }, []);
 
+  const changeInputValue = ({ target }) => {
+    setSearchArr(() =>
+      users.filter(
+        (name) => target.value !== '' && name.includes(target.value),
+      ),
+    );
+  };
+
   return (
-    <div>
-      <Title>
-        제목: <input type="text" ref={titleRef} />
-      </Title>
-      <ChargeSelect>
-        담당자: <Charge />
-      </ChargeSelect>
-      <EndDate>
-        마감일: <input type="datetime-local" ref={dueDateRef} />
-      </EndDate>
-      <Status>
-        상태:
-        <select>
-          {kanbanKeys.map((kanbanKey) => (
-            <option key={kanbanKey} selected={kanbanKey === modalKanbanStatus}>
-              {kanbanKey}
-            </option>
-          ))}
-        </select>
-      </Status>
-      <Content>
-        내용: <textarea>{content}</textarea>
-      </Content>
-      <ButtonContainer>
-        <CancelBtn onClick={cancelKanban}>취소</CancelBtn>
-        <SubmitBtn>저장</SubmitBtn>
-      </ButtonContainer>
-    </div>
+    <>
+      <ModalSide></ModalSide>
+      <ModalContent>
+        <Title>
+          제목: <input type="text" ref={titleRef} />
+        </Title>
+        담당자:
+        <Charge
+          searchArr={searchArr}
+          changeInputValue={changeInputValue}
+        ></Charge>
+        <EndDate>
+          마감일: <input type="datetime-local" ref={dueDateRef} />
+        </EndDate>
+        <Status>
+          상태:
+          <select>
+            {kanbanKeys.map((kanbanKey) => (
+              <option
+                key={kanbanKey}
+                selected={kanbanKey === modalKanbanStatus}
+              >
+                {kanbanKey}
+              </option>
+            ))}
+          </select>
+        </Status>
+        <Content>
+          내용: <textarea>{content}</textarea>
+        </Content>
+        <ButtonContainer>
+          <CancelBtn onClick={cancelKanban}>취소</CancelBtn>
+          <SubmitBtn>저장</SubmitBtn>
+        </ButtonContainer>
+      </ModalContent>
+    </>
   );
 };
 
-const Title = styled.div``;
+const ModalSide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background-color: #808080;
+  opacity: 0.5;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 30px;
+  width: 300px;
+  height: 300px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+`;
+
+const Title = styled.h4``;
 const ChargeSelect = styled.div``;
 const EndDate = styled.div``;
 const Status = styled.div``;
